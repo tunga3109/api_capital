83d3f5ba21f4715ae65996c1bcc6a2cc942614a7
Tung <tunga3109@gmail.com>
2023-02-03 05:31:24 +0200

    anv

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 token = '5556888362:AAEeI3EEspZM65LRV74nQdzXweDBLiT9GJA'
 
 
-API_KEY = 'QFSmCKkrSYlz6Igx' #expired 24 hours
+API_KEY = 'q2VZcVVpicuX6KA7' #expired 24 hours
 login = 'tunga3109@gmail.com'
 password = 'Xuxin_10031999'
--- test.py
+++ test.py
@@ -1,35 +1,10 @@
+from capitalcom import *
 import requests
-import json
-import sys
-import unittest
-
 from config import *
-from call import CST, X_SECURITY_TOKEN
 
 
-BASE_DEMO_URL = 'https://demo-api-capital.backend-capital.com' 
-BASE_LIVE_URL = 'https://api-capital.backend-capital.com'
-        
-'''Create session'''
-session = requests.Session()
+tung = Client(login, password, API_KEY)
+print(tung.place_the_position(direction=DirectionType.BUY, size=1, epic='BTCUSD'))
 
-def list_of_trades():
-    '''Show the list of trades'''    
-    response = session.get(
-        BASE_DEMO_URL + '/api/v1/positions',
-        headers={'CST': CST, 'X-SECURITY-TOKEN': X_SECURITY_TOKEN}
-        )
 
-    return len(response.json()["positions"])
-
-list_of_trades()
-
-class EndpointTestCase(unittest.TestCase):
-
-    def test_list_of_trades(self):
-        
-        
-        self.assertEqual(list_of_trades(), 0)
-
-unittest.main()
         
