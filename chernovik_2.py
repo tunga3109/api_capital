12732c4531f58c01f0d8a1cad813cea1cb1ef552
Tung <tunga3109@gmail.com>
2023-02-08 20:35:40 +0200

    leverage

--- chernovik_2.py
+++ chernovik_2.py
@@ -1,6 +1,14 @@
 from capitalcom.client_demo import *
 from config import *
 
+lev = {
+    "SHARES": 2,
+    "CURRENCIES": 30,
+    "INDICES": 50,
+    "CRYPTOCURRENCIES": 5,
+    "COMMODITIES": 10
+  }
+
 cl = Client(login, password, API_KEY)
-acc = cl.all_accounts()
+acc = cl.update_account_preferences(leverages=lev, hedgingmode=False)
 print(acc)
