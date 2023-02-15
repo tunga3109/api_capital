e6726d786214ae35a43b63c9fdaef9221eb0b1f8
Tung <tunga3109@gmail.com>
2023-02-15 20:19:02 +0200

    upt

--- chernovik_2.py
+++ chernovik_2.py
@@ -1,22 +1,7 @@
-from capitalcom import *
+from capitalcom.client_demo import *
 from config import *
-import json
-
 
 
 cl = Client(login, password, API_KEY)
-acc_switch = cl.switch_account(accountId='172785881488634142')
-acc_dict = json.loads(cl.all_accounts())
-print(json.dumps(acc_dict['accounts'], indent=4))
 
-# lev = {
-#     "SHARES": 2,
-#     "CURRENCIES": 30,
-#     "INDICES": 50,
-#     "CRYPTOCURRENCIES": 5,
-#     "COMMODITIES": 10
-#   }
 
-# cl = Client(login, password, API_KEY)
-# acc = cl.update_account_preferences(leverages=lev, hedgingmode=False)
-# print(acc)
