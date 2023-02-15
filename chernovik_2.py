dafa4109ae9e22b79de61fc1267b9e1f714fffbc
Tung <tunga3109@gmail.com>
2023-02-15 23:34:05 +0200

    Closed positions by using loops

--- chernovik_2.py
+++ chernovik_2.py
@@ -3,5 +3,9 @@ from config import *
 
 
 cl = Client(login, password, API_KEY)
+pos_dict = json.loads(cl.all_positions())['positions']
 
+for i in pos_dict:
+    dealId = i['position']['dealId']
+    cl.close_position(dealid=dealId)
 
