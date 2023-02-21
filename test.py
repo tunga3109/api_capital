3e1c4b454130a44852826d1f14f9c852a415b17c
Tung <tunga3109@gmail.com>
2023-02-21 14:12:04 +0200

    websocket api capital

--- test.py
+++ test.py
@@ -1,28 +1,30 @@
-from capitalcom.client_demo import *
-from config import *
-import requests
-import time
+import json
+import pprint
+import websockets
+import asyncio
 
-cl = Client(login, password, API_KEY)
-pos = cl.all_positions()
-pos_dict = json.loads(pos)
+from config import API_KEY as ApiKey
+from call import CST, X_SECURITY_TOKEN
 
-webhook_url = "https://webhook.site/token/75fe23d9-b6ac-46ba-810d-72b0bbd41a44/requests?sorted=newest"
 
-while True:
-    r = requests.get(webhook_url, headers={'Content-Type': 'application/json'})
+demo = "wss://api-streaming-capital.backend-capital.com/connect"
 
-    pos = json.loads(r.text)['data']
-    if len(pos) == 0:
-        time.sleep(0.5)
-        print(pos)
-    else:
-        body_params = json.loads(pos[-1]['content'])
-        time.sleep(1)
-        level = body_params['level']
-        direction = body_params['direction']
-        size = body_params['size']
-        type = body_params['type']
-        epic = body_params['epic']
-        print(cl.place_the_order(direction=DirectionType.BUY, epic=epic, size=size, level=level,type=OrderType.LIMIT))
-        break
+async def checkPrices():
+    async with websockets.connect(demo) as websocket:
+        req = {
+                "destination": "marketData.subscribe",
+                "correlationId": "1",
+                "cst": CST,
+                "securityToken": X_SECURITY_TOKEN,
+                "payload": { "epics": [ "BTCUSD"] }
+                    }
+        await websocket.send(json.dumps(req))
+        while True: 
+            response = json.loads(await websocket.recv())['payload']
+            pprint.pprint(response)
+
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(checkPrices())
