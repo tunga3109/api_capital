b2aba4be0a194a3ed01693fd09c0b2b17ae1d319
Tung <tunga3109@gmail.com>
2023-03-06 10:27:46 +0200

    test

--- chernovik.py
+++ chernovik.py
@@ -5,5 +5,5 @@ import json
 
 cl = Client(login, password, API_KEY)
 
-a = json.loads(cl.all_accounts())
-print(a['accounts'])
+a = cl.position_order_confirmation(deal_reference='o_7c0d3f1e-9b86-4e7f-9e4e-29cd669eebf4')
+print(a)
